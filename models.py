e40d422d92c52c591d6dc783e150bdc74321bcc2
ghostchinchilla <beca.mckay@yahoo.com>
2023-11-20 05:07:08 -0500

    updated models.py

--- models.py
+++ models.py
@@ -1,6 +1,7 @@
 """SQLAlchemy models for blogly."""
 
 from flask_sqlalchemy import SQLAlchemy
+import datetime
 
 db = SQLAlchemy()
 
@@ -22,6 +23,26 @@ class User(db.Model):
         """Return full name of user."""
 
         return f"{self.first_name} {self.last_name}"
+    
+    class Post(db.Model):
+    """Blog post."""
+
+    __tablename__ = "posts"
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.Text, nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    created_at = db.Column(
+        db.DateTime,
+        nullable=False,
+        default=datetime.datetime.now)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+
+    @property
+    def friendly_date(self):
+        """Return nicely-formatted date."""
+
+        return self.created_at.strftime("%a %b %-d  %Y, %-I:%M %p")
 
 
 def connect_db(app):
